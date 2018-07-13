b0df81215fcbf052674913a62185165f5878ee68
Dmytro Karpenko <dmitry.karpenko@gmail.com>
2018-07-13 23:28:44 +0300

    SolutionCalculator - autoformatted

--- Derivco.FullStack.Assignment/SolutionCalculator.cs
+++ Derivco.FullStack.Assignment/SolutionCalculator.cs
@@ -6,25 +6,27 @@
 
 namespace Derivco.FullStack.Assignment
 {
-  using System.Collections.Generic;
+    using System.Collections.Generic;
 
-  public class SolutionCalculator : ISolutionCalculator
-  {
-    #region ISolutionCalculator implementation
-    public Solution Calculate(IList<Rectangle> inputRectangles)
+    public class SolutionCalculator : ISolutionCalculator
     {
-      return new Solution
-      {
-        InputRectangles = inputRectangles,
-        OutputRectangles = CalculateSolution(inputRectangles)
-      };
-    }
-    #endregion
+        #region ISolutionCalculator implementation
+        public Solution Calculate(IList<Rectangle> inputRectangles)
+        {
+            return new Solution
+            {
+                InputRectangles = inputRectangles,
+                OutputRectangles = CalculateSolution(inputRectangles)
+            };
+        }
+        #endregion
 
-    private static IList<Rectangle> CalculateSolution(IList<Rectangle> inputRectangles)
-    {
-      // TODO: "Rotate" the input rectangles here.
-      return inputRectangles;
+        private static IList<Rectangle> CalculateSolution(IList<Rectangle> inputRectangles)
+        {
+            // TODO: "Rotate" the input rectangles here.
+            return inputRectangles;
+        }
+
+
     }
-  }
 }
