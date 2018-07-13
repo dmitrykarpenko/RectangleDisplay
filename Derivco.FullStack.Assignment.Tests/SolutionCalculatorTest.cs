98cfb9f4a6e9fb33a9fc93a42c1157f66385471d
Dmytro Karpenko <dmitry.karpenko@gmail.com>
2018-07-13 22:47:54 +0300

    initial

---
+++ Derivco.FullStack.Assignment.Tests/SolutionCalculatorTest.cs
@@ -0,0 +1,40 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="SolutionCalculatorTest.cs" company="Derivco">
+//   Copyright (C) Derivco 2017 All rights reserved
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Derivco.FullStack.Assignment.Tests
+{
+  using System.Collections.Generic;
+  using System.Diagnostics.CodeAnalysis;
+  using FluentAssertions;
+  using NUnit.Framework;
+
+  [TestFixture]
+  [ExcludeFromCodeCoverage]
+  [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Test methods.")]
+  public class SolutionCalculatorTest
+  {
+    [Test]
+    public void Calculate_GivenRectangles_ShouldCalculateCorrectSolution()
+    {
+      // Arrange
+      var calculator = new SolutionCalculator();
+      var inputRectangles = new List<Rectangle>
+      {
+        new Rectangle(2, 3),
+        new Rectangle(2, 2)
+      };
+      var expected = new Rectangle(4, 2);
+
+      // Act
+      Solution solution = calculator.Calculate(inputRectangles);
+
+      // Assert
+      solution.InputRectangles.ShouldAllBeEquivalentTo(inputRectangles);
+      solution.OutputRectangles.Count.Should().Be(1);
+      solution.OutputRectangles[0].ShouldBeEquivalentTo(expected);
+    }
+  }
+}
---
+++ Derivco.FullStack.Assignment/SolutionCalculator.cs
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="SolutionCalculator.cs" company="Derivco">
+//   Copyright (C) Derivco 2017 All rights reserved
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Derivco.FullStack.Assignment
+{
+  using System.Collections.Generic;
+
+  public class SolutionCalculator : ISolutionCalculator
+  {
+    #region ISolutionCalculator implementation
+    public Solution Calculate(IList<Rectangle> inputRectangles)
+    {
+      return new Solution
+      {
+        InputRectangles = inputRectangles,
+        OutputRectangles = CalculateSolution(inputRectangles)
+      };
+    }
+    #endregion
+
+    private static IList<Rectangle> CalculateSolution(IList<Rectangle> inputRectangles)
+    {
+      // TODO: "Rotate" the input rectangles here.
+      return inputRectangles;
+    }
+  }
+}
